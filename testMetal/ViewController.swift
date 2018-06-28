8da7f3a6bd2dd5ecb262f2c5cdc1b493081876e1
有本淳吾 <arimotojungo@arimotoatsushiwarenoimac.local>
2018-06-28 23:56:59 +0900

    Initial Commit

---
+++ testMetal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testMetal
+//
+//  Created by 有本淳吾 on 2018/06/28.
+//  Copyright © 2018 有本淳吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
